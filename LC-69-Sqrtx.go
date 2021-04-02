af2e17e6aaeefc6f59a06273f9ab4cc3e1a96dd3
george <george.yu@lightnet.io>
2021-04-02 11:33:33 +0800

    feat: add LC-69-Sqrtx.go

---
+++ LC-69-Sqrtx.go
@@ -0,0 +1,18 @@
+package LC_Go
+
+func mySqrt(x int) int {
+	ret := -1
+	start, end := 0, x
+
+	for start <= end {
+		mi := (start + end) / 2
+		if mi * mi <= x {
+			ret = mi
+			start = mi + 1
+		} else {
+			end = mi - 1
+		}
+	}
+
+	return ret
+}
